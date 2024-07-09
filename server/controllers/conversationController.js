97591b70088a4677000b2475ec819ab9e17cec1e
Nhan Nguyen Ngoc <sbtc.walkingdead@gmail.com>
2024-07-09 23:34:47 +0700

    update response error

--- server/controllers/conversationController.js
+++ server/controllers/conversationController.js
@@ -29,7 +29,7 @@ module.exports.singleConversation = async (req, res, next) => {
           }
         }
       }],
-      group: ['Conversation.id'],
+      group: ['conversations.id'],
       having: db.sequelize.literal(`COUNT(DISTINCT "users"."id") = 2`)
     });
 
